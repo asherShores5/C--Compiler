3ecf06c7906ff0f3a82fb6ce5096ba530885af85
asherShores5 <71547146+asherShores5@users.noreply.github.com>
2022-12-16 10:43:26 -0700

    Co-authored-by: ekliewer <ekliewer@users.noreply.github.com>

--- parser.tab.h
+++ parser.tab.h
@@ -1,8 +1,8 @@
-/* A Bison parser, made by GNU Bison 3.5.1.  */
+/* A Bison parser, made by GNU Bison 3.8.2.  */
 
 /* Bison interface for Yacc-like parsers in C
 
-   Copyright (C) 1984, 1989-1990, 2000-2015, 2018-2020 Free Software Foundation,
+   Copyright (C) 1984, 1989-1990, 2000-2015, 2018-2021 Free Software Foundation,
    Inc.
 
    This program is free software: you can redistribute it and/or modify
@@ -16,7 +16,7 @@
    GNU General Public License for more details.
 
    You should have received a copy of the GNU General Public License
-   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+   along with this program.  If not, see <https://www.gnu.org/licenses/>.  */
 
 /* As a special exception, you may create a larger work that contains
    part or all of the Bison parser skeleton and distribute that work
@@ -31,8 +31,9 @@
    This special exception was added by the Free Software Foundation in
    version 2.2 of Bison.  */
 
-/* Undocumented macros, especially those whose name start with YY_,
-   are private implementation details.  Do not rely on them.  */
+/* DO NOT RELY ON FEATURES THAT ARE NOT DOCUMENTED in the manual,
+   especially those whose name start with YY_ or yy_.  They are
+   private implementation details that can be changed or removed.  */
 
 #ifndef YY_YY_PARSER_TAB_H_INCLUDED
 # define YY_YY_PARSER_TAB_H_INCLUDED
@@ -44,46 +45,53 @@
 extern int yydebug;
 #endif
 
-/* Token type.  */
+/* Token kinds.  */
 #ifndef YYTOKENTYPE
 # define YYTOKENTYPE
   enum yytokentype
   {
-    ID = 258,
-    INTEGER = 259,
-    DECIMAL = 260,
-    SEMICOLON = 261,
-    EQ = 262,
-    WRITE = 263,
-    FUNC = 264,
-    IF = 265,
-    ELSE = 266,
-    WHILE = 267,
-    PLUS = 268,
-    MINUS = 269,
-    TIMES = 270,
-    DIVIDE = 271,
-    LOGICOP = 272,
-    LBRACKET = 273,
-    RBRACKET = 274,
-    LPAREN = 275,
-    RPAREN = 276,
-    LCURLY = 277,
-    RCURLY = 278,
-    INT = 279,
-    CHAR = 280,
-    FLOAT = 281,
-    RETURN = 282,
-    WRITELN = 283,
-    CHARACTER = 284
+    YYEMPTY = -2,
+    YYEOF = 0,                     /* "end of file"  */
+    YYerror = 256,                 /* error  */
+    YYUNDEF = 257,                 /* "invalid token"  */
+    ID = 258,                      /* ID  */
+    INTEGER = 259,                 /* INTEGER  */
+    DECIMAL = 260,                 /* DECIMAL  */
+    STRING = 261,                  /* STRING  */
+    SEMICOLON = 262,               /* SEMICOLON  */
+    EQ = 263,                      /* EQ  */
+    WRITE = 264,                   /* WRITE  */
+    SWRITE = 265,                  /* SWRITE  */
+    FUNC = 266,                    /* FUNC  */
+    IF = 267,                      /* IF  */
+    ELSE = 268,                    /* ELSE  */
+    WHILE = 269,                   /* WHILE  */
+    PLUS = 270,                    /* PLUS  */
+    MINUS = 271,                   /* MINUS  */
+    TIMES = 272,                   /* TIMES  */
+    DIVIDE = 273,                  /* DIVIDE  */
+    LOGICOP = 274,                 /* LOGICOP  */
+    LBRACKET = 275,                /* LBRACKET  */
+    RBRACKET = 276,                /* RBRACKET  */
+    LPAREN = 277,                  /* LPAREN  */
+    RPAREN = 278,                  /* RPAREN  */
+    LCURLY = 279,                  /* LCURLY  */
+    RCURLY = 280,                  /* RCURLY  */
+    INT = 281,                     /* INT  */
+    CHAR = 282,                    /* CHAR  */
+    FLOAT = 283,                   /* FLOAT  */
+    RETURN = 284,                  /* RETURN  */
+    WRITELN = 285,                 /* WRITELN  */
+    CHARACTER = 286                /* CHARACTER  */
   };
+  typedef enum yytokentype yytoken_kind_t;
 #endif
 
 /* Value type.  */
 #if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
 union YYSTYPE
 {
-#line 41 "parser.y"
+#line 46 "parser.y"
 
 	//Tokens declared in C so we can declare them in C--... I think
 	int number;
@@ -92,7 +100,7 @@ union YYSTYPE
 	char* string;
 	struct AST* ast;
 
-#line 96 "parser.tab.h"
+#line 104 "parser.tab.h"
 
 };
 typedef union YYSTYPE YYSTYPE;
@@ -103,6 +111,8 @@ typedef union YYSTYPE YYSTYPE;
 
 extern YYSTYPE yylval;
 
+
 int yyparse (void);
+
 
 #endif /* !YY_YY_PARSER_TAB_H_INCLUDED  */
